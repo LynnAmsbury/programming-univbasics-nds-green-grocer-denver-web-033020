d6ee25cf93ddea48416202f90efad3a6000dffac
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-03-16 19:52:30 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -107,7 +107,7 @@ def checkout(cart, coupons)
   # * consolidate_cart
   # * apply_coupons
   # * apply_clearance
-  #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
+  consolidated_cart = consolidate_cart(cart)
 end
