79b983a56b205df7a58034394127b82fd1bfe69f
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-03-16 18:36:15 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -54,7 +54,7 @@ def apply_coupons(cart, coupons)
     couponed_item_name = "${coupons[coupon_counter][:item]} W/COUPON"
     cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_name, cart)
     if cart_item && cart_item[:count] >= coupons[coupon_counter][:num]
-      
+
     end
     coupon_counter += 1
   end
