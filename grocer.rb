faedaa4126092df58c6140c3a6e58de4979a7a68
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-03-16 16:52:17 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -22,6 +22,11 @@ def consolidate_cart(cart)
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
   new_cart = []
+  item_counter_index = 0
+  while item_counter_index < collection.length do
+    
+  item_counter_index += 1
+  end
 end
 
 def apply_coupons(cart, coupons)
