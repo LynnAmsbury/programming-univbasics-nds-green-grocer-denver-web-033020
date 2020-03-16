2b417e971fc990c7cafedeb0a7747f1b40102881
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-03-16 19:14:27 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -91,8 +91,9 @@ def apply_clearance(cart)
   #
   # REMEMBER: This method **should** update cart
   counter = 0
-  while condition
-
+  while counter < cart.length
+    
+    counter += 1
   end
 end
 
