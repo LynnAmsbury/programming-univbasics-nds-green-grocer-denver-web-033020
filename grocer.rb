803468fc753bb7d55c7f0fae3609bb6150420c31
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-03-16 20:19:32 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -116,7 +116,7 @@ def checkout(cart, coupons)
   total = 0
   counter = 0
   while final_cart.length do
-
+    total += final_cart[counter][:price] * final_cart[counter][:count]
     counter += 1
   end
 end
