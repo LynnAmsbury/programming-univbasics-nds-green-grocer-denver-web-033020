f9b068739f48a594bfc648bbb3e7a86b3f8c1462
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-03-16 18:47:09 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -60,7 +60,7 @@ def apply_coupons(cart, coupons)
       else
         cart_item_with_coupon = {
           :item => couponed_item_name,
-          :price => coupons[coupon_counter][:cost] / coupons[coupon_counter][:num]
+          :price => coupons[coupon_counter][:cost] / coupons[coupon_counter][:num],
           :clearance =>
           :count => 1
         }
