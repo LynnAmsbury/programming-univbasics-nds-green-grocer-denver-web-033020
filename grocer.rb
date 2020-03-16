4db9fd7b9b30712e0ffa8173957697454731be12
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-03-16 14:58:28 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -5,6 +5,16 @@ def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
+  i = 0 
+  new_collection = {}
+  while i < collection.length do 
+    if collection[i][:item] == name
+      return collection[i]
+    else 
+      nil
+    end
+    i += 1 
+  end 
 end
 
 def consolidate_cart(cart)
