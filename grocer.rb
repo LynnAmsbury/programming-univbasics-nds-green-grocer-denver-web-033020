d41579f47e918b596746ff3466ebf19999ec4026
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-03-16 16:38:11 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -8,7 +8,9 @@ def find_item_by_name_in_collection(name, collection)
   # Find a specific item using its name inside of the collection array
   item_counter_index = 0
   while item_counter_index < collection.length do
-    collection[item_counter_index]
+    if collection[item_counter_index][:item]
+      
+    end
     item_counter_index += 1
   end
 end
